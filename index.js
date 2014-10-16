c8469056db9c3c641d97273f7e98395ff9400679
James Halliday <mail@substack.net>
2014-10-16 16:33:36 +0800

    exclusive ranges

--- index.js
+++ index.js
@@ -3,16 +3,34 @@ var defined = require('defined');
 module.exports = function (opts, prefix) {
     if (!opts) opts = {};
     var xopts = {};
+    var gte = defined(prefix.gte, prefix.ge, prefix.start);
+    var lte = defined(prefix.lte, prefix.le, prefix.end);
     
-    if (defined(opts.gte, opts.ge) !== undefined) {
-        xopts.gte = prefix.gt(defined(opts.gte, opts.ge));
+    if (prefix.gt) {
+        if (defined(opts.gte, opts.ge) !== undefined) {
+            xopts.gte = prefix.gt(defined(opts.gte, opts.ge));
+        }
+        else xopts.gt = prefix.gt(opts.gt);
     }
-    else xopts.gt = prefix.gt(opts.gt);
+    else if (gte) {
+        if (defined(opts.gte, opts.ge) !== undefined) {
+            xopts.gte = gte(defined(opts.gte, opts.ge));
+        }
+        else xopts.gt = gte(opts.gt);
+    }
     
-    if (defined(opts.lte, opts.le) !== undefined) {
-        xopts.lte = prefix.lt(defined(opts.lte, opts.le));
+    if (prefix.lt) {
+        if (defined(opts.lte, opts.le) !== undefined) {
+            xopts.lte = prefix.lt(defined(opts.lte, opts.le));
+        }
+        else xopts.lt = prefix.lt(opts.lt);
     }
-    else xopts.lt = prefix.lt(opts.lt);
+    else if (lte) {
+        if (defined(opts.lte, opts.le) !== undefined) {
+            xopts.lte = lte(defined(opts.lte, opts.le));
+        }
+        else xopts.lt = lte(opts.lt);
+    }
     
     return xopts;
 };
