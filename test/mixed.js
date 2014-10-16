a5e7b735b09814afe47ff43f38b72de7252c4ba8
James Halliday <mail@substack.net>
2014-10-16 16:40:42 +0800

    test coverage for gte/lte+gte/lte case

--- test/mixed.js
+++ test/mixed.js
@@ -66,3 +66,16 @@ test('mixed ge/le', function (t) {
         lte: [ 'post', 'sub\uffff' ]
     });
 });
+
+test('mixed gte/lte gte/lte', function (t) {
+    t.plan(1);
+    var params = { gte: 'sub', lte: 'sub\uffff' };
+    var opts = wrap(params, {
+        gte: function (x) { return [ 'post', defined(x, null) ] },
+        lte: function (x) { return [ 'post', defined(x, undefined) ] }
+    });
+    t.deepEqual(opts, {
+        gte: [ 'post', 'sub' ],
+        lte: [ 'post', 'sub\uffff' ]
+    });
+});
